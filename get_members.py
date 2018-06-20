dd7ca738e06842f70f8b78ebfaac667bdb8df693
Michael Bosworth <michael.a.bosworth@gmail.com>
2018-06-20 12:10:45 -0400

    CSV

--- get_members.py
+++ get_members.py
@@ -13,12 +13,12 @@ if len(sys.argv) < 3:
   print("")
   print("2. Go to https://secure.meetup.com/meetup_api/key/ and copy your API key.")
   print("")
-  print("3. Rerun this command with the group's URL name as the first parameter and the API key as the second. For example:")
-  print("{} Blockchain-for-Funds 42ccc44912e4414b9c5d2806f74197".format(sys.argv[0]))
+  print("3. Rerun this command with the group's URL name as the second parameter and the API key as the first. For example:")
+  print("{} 42ccc44912e4414b9c5d2806f74197 Blockchain-for-Funds".format(sys.argv[0]))
   sys.exit(1)
 
-group_name = sys.argv[1]
-user_key = sys.argv[2]
+user_key = sys.argv[1]
+group_name = sys.argv[2]
 
 url="https://api.meetup.com/members?group_urlname={group_name}&sign=true&key={user_key}".format(group_name=group_name, user_key=user_key)
 
@@ -45,5 +45,19 @@ html_file = open(html_file_path, 'w')
 html_file.write(html_output)
 html_file.close()
 
-call(['open', html_file_path])
+# call(['open', html_file_path])
+
+def to_row(p):
+  assert "".join([p['name'], p['link'], p['photo_url'], p['country'], p['city'], p['zip'] ]).find('"') == -1, 'Found data with a double-quote, tell Michael there is a problem'
+  return '"{}"'.format('","'.join([p['name'], p['link'], p['photo_url'], p['country'], p['city'], p['zip'] ]))
+
+csv_output = '"Name","Link","Photo","Country","City","Zip"\n{}'.format('\n'.join([to_row(p) for p in people]))
+
+csv_file_path = path.expanduser("~/Downloads/{}.{}.csv".format(group_name, int(time.time())))
+with open(csv_file_path, 'w') as csv_file:
+  csv_file.write(csv_output)
+
+print("Saved CSV to {}".format(csv_file_path))
+
+call(['open', csv_file_path])
 
